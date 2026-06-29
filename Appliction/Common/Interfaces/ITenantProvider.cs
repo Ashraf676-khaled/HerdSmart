3b1cfaf61f42e55e542ca223d5cfceac4d4f5c72
Ashraf676-khaled <ashrafkhaled2007@gmail.com>
2026-06-29 15:35:39 +0300

    Create ITenant provuder and Queryable Extintion

---
+++ Appliction/Common/Interfaces/ITenantProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface ITenantProvider
+    {
+        Ulid GetTenantId ();
+        Guid GetUserId();
+        String GetUserRole ();
+        bool IsAuthenticated();
+
+    }
+}
